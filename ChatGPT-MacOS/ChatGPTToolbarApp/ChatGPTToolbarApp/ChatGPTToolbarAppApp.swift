6c406d3c3b4b7d2cb616f7e26681bb9136a7b673
bhishaksanyal <sanyalbishak93@gmail.com>
2023-03-20 21:06:04 +0530

    Completed toolbar app

--- ChatGPT-MacOS/ChatGPTToolbarApp/ChatGPTToolbarApp/ChatGPTToolbarAppApp.swift
+++ ChatGPT-MacOS/ChatGPTToolbarApp/ChatGPTToolbarApp/ChatGPTToolbarAppApp.swift
@@ -9,9 +9,42 @@ import SwiftUI
 
 @main
 struct ChatGPTToolbarAppApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "info.circle.fill", accessibilityDescription: "Information")
+            statusButton.action = #selector(togglePopover)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 500, height: 500)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: ContentView())
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
         }
     }
 }
