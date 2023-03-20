7a555c68d0b83bb9da8052141a3d19f69bce6bc7
bhishaksanyal <sanyalbishak93@gmail.com>
2023-03-20 20:10:36 +0530

    Initial setup for ChatGPT toolbar app

---
+++ ChatGPT-MacOS/ChatGPTToolbarApp/ChatGPTToolbarApp/ChatGPTToolbarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTToolbarAppApp.swift
+//  ChatGPTToolbarApp
+//
+//  Created by Bhishak Sanyal on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTToolbarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
