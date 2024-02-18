8bfda6ea51b9ff7125cc164b2ad0bb48b223c0f4
bhishaksanyal <sanyalbishak93@gmail.com>
2024-02-18 20:56:56 +0530

    drawing-paths-and-shapes - Completed

---
+++ Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Bhishak Sanyal on 18/02/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
