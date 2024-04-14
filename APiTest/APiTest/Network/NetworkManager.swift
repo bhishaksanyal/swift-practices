aa781204a970bfefceddec15354d2719e6cbb33f
bhishaksanyal <sanyalbishak93@gmail.com>
2024-04-14 23:01:40 +0530

    Api call network manager added

---
+++ APiTest/APiTest/Network/NetworkManager.swift
@@ -0,0 +1,78 @@
+//
+//  NetworkManager.swift
+//  APiTest
+//
+//  Created by Bhishak Sanyal on 13/04/24.
+//
+
+import Foundation
+import UIKit
+import OSLog
+
+let BASE_URL = "https://streaming-availability.p.rapidapi.com/"
+
+func log(_ value: Any,_ key: String = "") {
+    print("\n \(key)-----> \n\(value) \n")
+}
+
+struct APIError: Error, Codable {
+    var statusCode: Int?
+    let message: String?
+}
+
+enum URLType {
+    case getCountries
+    case noURLType
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func fetchData(urlType: URLType) async throws -> ( Data?, APIError? ) {
+        
+        var urlString = ""
+        var httpMethod: String? = nil
+        
+        switch urlType {
+            case .getCountries:
+                urlString = BASE_URL + "countries"
+                httpMethod = "GET"
+            case .noURLType:
+                urlString = ""
+        }
+        
+        log(urlString)
+        
+        guard let url = URL(string: urlString) else {
+            return (nil , APIError(statusCode: -1, message: "Invalid URL"))
+        }
+        
+        var urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 30)
+        urlRequest.httpMethod = httpMethod
+        urlRequest.setValue("#key", forHTTPHeaderField: "X-RapidAPI-Key")
+        urlRequest.setValue("streaming-availability.p.rapidapi.com", forHTTPHeaderField: "X-RapidAPI-Host")
+        
+        do {
+            let ( data, response ) = try await URLSession.shared.data(for: urlRequest)
+            
+            log(data)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if (httpResponse.statusCode != 200) {
+                    var error = try JSONDecoder().decode(APIError.self, from: data)
+                    error.statusCode = httpResponse.statusCode
+                    return (nil, error)
+                } else {
+                    // Success
+                    return (data, nil)
+                }
+            }
+            
+        } catch {
+            log(error, "ERROR")
+        }
+        
+        return (nil, nil)
+    }
+    
+}
--- APiTest/APiTest/ViewController.swift
+++ APiTest/APiTest/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let network = NetworkManager()
+        
+        Task {
+            do {
+                let result: APIError = try await network.fetchData(urlType: .getCountries)
+                print("CODE ====== \(result.statusCode ?? 0)\n")
+                print("MESSAGE ====== \(result.message ?? "nothing")\n")
+            } catch {
+                print("Error", error)
+            }
+        }
     }
-
-
+    
+    
 }
 
