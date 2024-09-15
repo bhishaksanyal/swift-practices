576fd186c4f29ab59e220dffbb40bf9654ce7ddc
bhishaksanyal <sanyalbishak93@gmail.com>
2024-09-15 21:47:00 +0530

    MyTask - App initial commit

---
+++ MyTask/MyTask/MyTaskApp.swift
@@ -0,0 +1,19 @@
+//
+//  MyTaskApp.swift
+//  MyTask
+//
+//  Created by Bhishak Sanyal on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
